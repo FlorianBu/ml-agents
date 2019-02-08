51bb6ddfd74da84d45f9f1cfd465a87de8124e55
unknown <florian-butsch@gmx.de>
2019-02-08 21:06:05 +0100

    upgraded environment to unity ml v0.6

---
+++ UnitySDK/Assets/InstantiateEnv.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateEnv : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform GameTable;
+    public int TableAmount;
+    void Start()
+    {
+        for (int i = 1; i < TableAmount; i++)
+        {
+            Instantiate(GameTable, new Vector3(0, 5*i, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
